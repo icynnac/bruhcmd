6cb680c7b94375222f75e943c2728793c342a91d
icynnac <68450404+icynnac@users.noreply.github.com>
2020-11-25 19:05:44 -0500

    BruhCMD 1.2 Update

--- src/me/Icynnac/bruhcmd/Main.java
+++ src/me/Icynnac/bruhcmd/Main.java
@@ -1,12 +1,42 @@
 package me.Icynnac.bruhcmd;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import me.Icynnac.bruhcmd.commands.BruhCommand;
 
+@SuppressWarnings("unused")
 public class Main extends JavaPlugin {
 
 	@Override
 	public void onEnable(){
-		new BruhCommand(this);
+		this.saveDefaultConfig();
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (label.equalsIgnoreCase("bruh")) {
+			if (!sender.hasPermission("bruhcmd.reload")) {
+				sender.sendMessage(ChatColor.RED + "You cannot run this command, sorry!");
+				return true;
+			}
+			if (args.length == 0) {
+				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("configuration.messages.bruh-message")));
+				return true;
+			}
+			if (args.length > 0) {
+				if (args[0].equalsIgnoreCase("reload")) {
+					sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("configuration.messages.reload-message")));
+					this.reloadConfig();
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
