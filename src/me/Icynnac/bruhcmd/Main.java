f1b0d1103242fc927ab46e1d29d527bdda2ab03c
icynnac <68450404+icynnac@users.noreply.github.com>
2020-12-02 17:30:10 -0500

    Version 1.2.5 Update

--- src/me/Icynnac/bruhcmd/Main.java
+++ src/me/Icynnac/bruhcmd/Main.java
@@ -4,14 +4,16 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("unused")
-public class Main extends JavaPlugin {
+public final class Main extends JavaPlugin {
 
 	@Override
 	public void onEnable(){
 		this.saveDefaultConfig();
+    
 	}
 	
 	@Override
@@ -21,19 +23,41 @@ public class Main extends JavaPlugin {
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (label.equalsIgnoreCase("bruh")) {
-			if (!sender.hasPermission("bruhcmd.reload")) {
-				sender.sendMessage(ChatColor.RED + "You cannot run this command, sorry!");
-				return true;
+			if (args.length > 0) {
+				if (args[0].equalsIgnoreCase("reload")) {
+					if (sender.hasPermission("bruhcmd.reload")) {
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("configuration.messages.reload-message")));
+						this.reloadConfig();
+						return true;
+					} else {
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("configuration.messages.no-permission-message")));
+					}
+				}
 			}
 			if (args.length == 0) {
 				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("configuration.messages.bruh-message")));
 				return true;
 			}
 			if (args.length > 0) {
-				if (args[0].equalsIgnoreCase("reload")) {
-					sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("configuration.messages.reload-message")));
-					this.reloadConfig();
-					return true;
+				if (args[0].equalsIgnoreCase("about")) {
+					if (sender.hasPermission("bruhcmd.about")) {
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b--------BruhCMD--------"));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&bVersion: &f1.2.5"));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&bDeveloper: &fIcynnac"));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&bCommands:"));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b/bruh"));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&fMain Command of the plugin."));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b/bruh reload"));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cAdministrator Command"));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&fReloads the config.yml."));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b/bruh about"));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cAdministrator Command"));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&fShows this menu :D"));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b------------------------"));
+						return true;
+					} else {
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("configuration.messages.no-permission-message")));
+					}
 				}
 			}
 		}
