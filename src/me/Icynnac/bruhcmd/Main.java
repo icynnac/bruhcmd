02a8aeafe0ad0ee903a499bbb01afb2291389ee2
icynnac <68450404+icynnac@users.noreply.github.com>
2021-02-27 22:41:08 -0500

    BruhCMD // 1.5.1 Changes

--- src/me/Icynnac/bruhcmd/Main.java
+++ src/me/Icynnac/bruhcmd/Main.java
@@ -1,13 +1,21 @@
 package me.icynnac.bruhcmd;
 
+import me.icynnac.bruhcmd.gui.NewBruhAbout;
+import me.icynnac.bruhcmd.gui.Events;
 import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.util.Vector;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 @SuppressWarnings("unused")
@@ -17,6 +25,20 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable(){
+        getCommand("bruhcmd").setExecutor(new NewBruhAbout());
+        getServer().getPluginManager().registerEvents(new Events(), this);
+        if (Bukkit.getVersion().contains("1.12.2")) {
+            BukkitScheduler scheduler = getServer().getScheduler();
+            scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                @Override
+                public void run() {
+                    Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "BruhCMD >> " + ChatColor.RED + "You're running an older version of spigot, some things in BruhCMD");
+                    Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "BruhCMD >> " + ChatColor.RED + "may be changed/broken. Submit any bug reports to my GitHub issues page:");
+                    Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "BruhCMD >> " + ChatColor.AQUA + "https://github.com/icynnac/bruhcmd/issues");
+                }
+            }, 20*10);
+        }
+
         this.saveDefaultConfig();
 
         Logger logger = this.getLogger();
@@ -32,7 +54,6 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onDisable(){
-
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -51,29 +72,6 @@ public final class Main extends JavaPlugin {
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("configuration.messages.bruh-message")));
                 return true;
             }
-            if (args.length > 0) {
-                if (args[0].equalsIgnoreCase("about")) {
-                    if (sender.hasPermission("bruhcmd.about")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b--------BruhCMD--------"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&bVersion: &f1.4"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&bDeveloper: &fIcynnac"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ""));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b- /bruh"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b- /bruh reload"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cAdministrator Command"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b- /bruh about"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cAdministrator Command"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b- /cutg"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cAdministrator Command"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b- /pop"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b- /yeet"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cAdministrator Command"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b------------------------"));
-                    } else {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("configuration.messages.no-permission-message")));
-                    }
-                }
-            }
         }
 
         if (label.equalsIgnoreCase("cutg")) {
@@ -87,11 +85,16 @@ public final class Main extends JavaPlugin {
                         @Override
                         public void run() {
                             sender.sendMessage(ChatColor.RED + "I like ya cut G.");
+                        }
+                    }, 20*3);
+                    scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                        @Override
+                        public void run() {
                             Player p = (Player) sender;
                             p.setHealth(0);
                             sender.sendMessage("<" + p.getDisplayName() + "> AAAAAAAAAAAAAAAAAAAAAAAA");
                         }
-                    }, 20*3);
+                    }, 20*5);
                 }
             } else {
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("configuration.messages.no-permission-message")));
@@ -126,6 +129,67 @@ public final class Main extends JavaPlugin {
 
                     p.setVelocity(v);
                     p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b&lBruhCMD &8&l>>  &cYEET"));
+                }
+            }
+        }
+        if (label.equalsIgnoreCase("sus")) {
+            if (sender.hasPermission("bruhcmd.sus")) {
+                if (!(sender instanceof Player)) {
+                    sender.sendMessage(ChatColor.RED + "BruhCMD >> Hey! You can't use this command in console.");
+                } else {
+                    Player p = (Player) sender;
+                    p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,20*5, 2));
+                    p.sendTitle(ChatColor.RED + "Impostor", "");
+                    Location loc = p.getLocation();
+                    p.playNote(loc, Instrument.PIANO, Note.natural(1, Note.Tone.E));
+                    ItemStack sushat = new ItemStack(Material.LEATHER_HELMET);
+                    ItemStack susshirt = new ItemStack(Material.LEATHER_CHESTPLATE);
+                    ItemStack suspant = new ItemStack(Material.LEATHER_LEGGINGS);
+                    ItemStack susshoe = new ItemStack(Material.LEATHER_BOOTS);
+
+                    LeatherArmorMeta hatmeta = (LeatherArmorMeta) sushat.getItemMeta();
+                    hatmeta.setColor(Color.RED);
+                    sushat.setItemMeta(hatmeta);
+
+                    LeatherArmorMeta shirtmeta = (LeatherArmorMeta) susshirt.getItemMeta();
+                    shirtmeta.setColor(Color.RED);
+                    susshirt.setItemMeta(shirtmeta);
+
+                    LeatherArmorMeta pantmeta = (LeatherArmorMeta) suspant.getItemMeta();
+                    pantmeta.setColor(Color.RED);
+                    suspant.setItemMeta(pantmeta);
+
+                    LeatherArmorMeta shoemeta = (LeatherArmorMeta) susshoe.getItemMeta();
+                    shoemeta.setColor(Color.RED);
+                    susshoe.setItemMeta(shoemeta);
+
+                    p.getEquipment().setHelmet(sushat);
+                    p.getEquipment().setChestplate(susshirt);
+                    p.getEquipment().setLeggings(suspant);
+                    p.getEquipment().setBoots(susshoe);
+                    BukkitScheduler scheduler = getServer().getScheduler();
+                    scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                        @Override
+                        public void run() {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&f<&9Blue&f> red sus"));
+                            p.playNote(loc, Instrument.SNARE_DRUM, Note.natural(1, Note.Tone.A));
+                        }
+                    }, 20*7);
+                    scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                        @Override
+                        public void run() {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&f<&c" + sender.getName() + "&f> no"));
+                            p.playNote(loc, Instrument.SNARE_DRUM, Note.natural(1, Note.Tone.A));
+                        }
+                    }, 20*9);
+                    scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                        @Override
+                        public void run() {
+                            Player p = (Player) sender;
+                            p.setHealth(0);
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c" + p.getName() + "&f was the &cImpostor."));
+                        }
+                    }, 20*12);
                 }
             }
         }
